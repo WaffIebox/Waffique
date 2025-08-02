6d2937cc9ecfc0aaa52964b8166305825daf4baf
waffie <wbeals202@gmail.com>
2025-08-02 11:34:23 -0600

    Whatever I forgot to push

--- waffique/src/App.js
+++ waffique/src/App.js
@@ -1,11 +1,17 @@
 import Header from './components/Header.jsx';
 import './styles/App.css';
+import './styles/index.css';
 
 function App() {
   return (
-  <div>
+
+    <div className="bg-darkBlue min-h-screen">
+
       <Header />
-  </div>
+
+
+
+</div>
   );
 }
 
